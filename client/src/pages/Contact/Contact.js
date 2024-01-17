93d40733c6654cca5f1c9213b765a6c60524b7ef
nrezhang <henryszhang83@gmail.com>
2024-01-17 12:20:25 -0500

    routes

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -20,7 +20,7 @@ const Contact = () => {
         toast.error('Please enter a valid email')
       }
       else{
-        const res = await axios.post('http://localhost:8080/api/v1/portfolio/sendEmail', {name, email, msg})   //ISSUES
+        const res = await axios.post('/api/v1/portfolio/sendEmail', {name, email, msg})   //ISSUES
         if(res.data.success){
           toast.success('Your Message was Sent')
           setName('')
