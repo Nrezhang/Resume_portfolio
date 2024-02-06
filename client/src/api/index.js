80c6b9128052d2fb7062319ccc6e30840ccde80e
nrezhang <henryszhang83@gmail.com>
2024-02-06 00:04:36 -0500

    vercel api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -23,7 +23,7 @@ app.use(cors(
 app.use(express.json());
 
 //static files
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 //route
 
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 //port
